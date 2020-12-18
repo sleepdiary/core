e7958dcc24653ee3bab13bcbfe207dd6165d532a
Andrew Sayers <andrew-github.com@pileofstuff.org>
2020-12-18 14:09:33 +0000

    Add Standard format

---
+++ src/Standard/README.md
@@ -0,0 +1,83 @@
+# Standardised Diary Format
+
+Common baseline format for sleep diaries.  This format was created as part of this project, to provide baseline functionality between different formats.  It does not try to represent every feature in every format.  The format also includes data that can be easily calculated, even if it is not present in the source file format.  This may make it easier to produce some graphs and statistics.
+
+This file is the format's official documentation.
+
+## File format
+
+Data is stored in a JavaScript object.  It can be serialised to and from JSON.
+
+## `minimum_day_duration`
+
+This record indicates the minimum expected day duration in milliseconds.  By default, day numbers are calculated by looking for "asleep" events at least this far apart.
+
+
+## `maximum_day_duration`
+
+This record indicates the maximum expected day duration in milliseconds.  By default, missing records are detected by looking for "asleep" events at least this far apart.
+
+## `records`
+
+An array of records, indicating the user's status during a given period.  Each record is an object with values described below.
+
+### `start`
+
+Unix time in milliseconds when the record started.
+
+### `start_timezone`
+
+User's timezone at the time referred to by the `start` record
+
+### `end`
+
+Unix time in milliseconds before which the record ended.  This should be a string from the [tz database](https://en.wikipedia.org/wiki/Tz_database).  The database includes several timezones of the form `Etc/GMT[+-]N` for use when converting from formats that store times as an offset from GMT.
+
+### `end_timezone`
+
+User's timezone at the time referred to by the `end` record.  Should be a string from the [tz database](https://en.wikipedia.org/wiki/Tz_database).  The database includes several timezones of the form `Etc/GMT[+-]N` for use when converting from formats that store times as an offset from GMT.
+
+### `duration`
+
+Duration of the record in milliseconds.  This is normally equal to `end - start`, and should not be greater than that amount.  But some formats will record an interruption (e.g. a period of wakefulness during sleep) by altering the duration without altering the start or end times.
+
+### `status`
+
+Status associated with the record (usually `awake` or `asleep`).  See `DiaryStandardRecordStatus` in [format.js](format.js) for the complete list.
+
+### `tags`
+
+An array of tags associated with this record.  Each value must be a string.
+
+### `comments`
+
+An array of comments associated with this record.  Each value can either be a string (for a comment with no associated time) or an object with `time` and `text` members (for timestamped comments).
+
+### `day_number`
+
+(Estimated) day number associated with the record.  This may be useful for creating graphs, but should not be relied on for statistics.
+
+Most formats do not store day-related values explicitly, so this value is usually inferred from the data.  In some cases, these inferences may be incorrect - for example, the day number is increased by a maximum of 2 at a time, no matter how large the gap between two records.  In other cases, they may not be meaningful at all - for example, people with polyphasic sleep might not have a meaningful concept of days at all.  Days typically begin at the start of an "asleep" event, because sleep is the only record type guaranteed to be in a sleep diary.
+
+### `start_of_new_day`
+
+Boolean value indicating whether the current day number is different to the previous record's day number.  This may be useful for creating graphs, but should not be relied on for statistics.
+
+### `is_primary_sleep`
+
+Boolean value indicating whether this appears to be the primary sleep for the current day.  This may be useful for creating graphs, but should not be relied on for statistics.
+
+Most formats do not differentiate between types of sleep, so this value is usually inferred from the data.  The inference is currently done by looking for the longest sleep in each day.
+
+### `missing_record_after`
+
+Boolean value indicating whether the user appears to have missed a record after the current one.  This may be useful for creating graphs, but should not be relied on for statistics.
+
+Most formats do not detect this automatically, so this value is usually inferred from the data.  The inference is currently done by looking for two `asleep` records with no `awake` record between them, or vice versa.
+
+# In this directory
+
+You may find the following useful:
+
+* [JavaScript example code](format.js)
+* [Test cases](tests/)
---
+++ src/Standard/format.js
@@ -0,0 +1,532 @@
+/*
+ * Copyright 2020 Andrew Sayers <andrew-github.com@pileofstuff.org>
+ * 
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use, copy,
+ * modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+ * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+"use strict";
+
+/**
+ * Valid record statuses
+ * @enum {string}
+ */
+const DiaryStandardRecordStatus = {
+    /** user is currently awake */
+    awake : "awake" ,
+    /** user is in bed but not asleep */
+    in_bed: "in bed",
+    /** user is asleep */
+    asleep: "asleep",
+};
+
+/**
+ * @typedef {{
+ *   start               : number,
+ *   end                 : number,
+ *   start_timezone      : string,
+ *   end_timezone        : string,
+ *   duration            : (number|null|undefined),
+ *   status              : DiaryStandardRecordStatus,
+ *   tags                : Array<string>,
+ *   comments            : Array<string|{time:number,text:string}>,
+ *   day_number          : number,
+ *   start_of_new_day    : boolean,
+ *   is_primary_sleep    : boolean,
+ *   missing_record_after: boolean
+ * }} DiaryStandardRecord
+ *
+ * A single record in a diary (e.g. one sleep) - see README.md for details
+ *
+ */
+let DiaryStandardRecord;
+
+
+/**
+ * @public
+ * @unrestricted
+ * @augments DiaryBase
+ *
+ * @example
+ * let diary = new_sleep_diary(contents_of_my_file));
+ *
+ * // print the minimum expected day duration in milliseconds:
+ * console.log(diary.minimum_day_duration);
+ * -> 12345
+ *
+ * // print the maximum expected day duration in milliseconds:
+ * console.log(diary.maximum_day_duration);
+ * -> 23456
+ *
+ * // Print the complete list of records
+ * console.log(diary.records);
+ * -> [
+ *      {
+ *        // DiaryStandardRecordStatus value, usually "awake" or "asleep"
+ *        status: "awake",
+ *
+ *        // start and end time (in milliseconds past the Unix epoch), estimated if the user forgot to log some data:
+ *        start: 12345678,
+ *        end: 23456789,
+ *        start_timezone: "Etc/GMT-1",
+ *        end_timezone: "Europe/Paris",
+ *
+ *        duration: 11111111, // or a falsey value if duration is unknown
+ *
+ *        // tags associated with this period:
+ *        tags: [
+ *          "tag 1",
+ *          "tag 2",
+ *          ...
+ *        ],
+ *
+ *        // comments recorded during this period:
+ *        comments: [
+ *          "comment with no associated timestamp",
+ *          { time: 23456543, text: "timestamped comment" },
+ *          ...
+ *        ],
+ *
+ *        // (estimated) day this record is assigned to:
+ *        day_number: 1,
+ *
+ *        // true if the current day number is greater than the previous record's day number:
+ *        start_of_new_day: true,
+ *
+ *        // whether this value is the primary sleep for the current day number:
+ *        is_primary_sleep: false,
+ *
+ *        // this is set if it looks like the user forgot to log some data:
+ *        missing_record_after: true
+ *
+ *      },
+ *
+ *      ...
+ *
+ *    ]
+ *
+ * // Print the user's current sleep/wake status:
+ * console.log(diary.latest_sleep_status());
+ * -> "awake"
+ *
+ * // Print the user's sleep statistics:
+ * console.log( diary.summarise_records( record => record.status == "asleep" ) );
+ * -> {
+ *                    average           : 12345.678,
+ *                    mean              : 12356.789,
+ *      interquartile_mean              : 12345.678,
+ *                    standard_deviation: 12.56,
+ *      interquartile_standard_deviation: 12.45,
+ *                    median            : 12345,
+ *      interquartile_range             : 12,
+ *                    durations         : [ undefined, 12345, null, ... ],
+ *      interquartile_durations         : [ 10000, 10001 ... 19998, 19999 ],
+ *    }
+ *
+ * // Print the user's day length statistics for the past 30 days:
+ * let cutoff = new Date().getTime() - 1000*60*60*24*30;
+ * console.log( diary.summarise_days( record => record.start > cutoff ) );
+ * -> {
+ *                    average           : 12345.678,
+ *                    mean              : 12356.789,
+ *      interquartile_mean              : 12345.678,
+ *                    standard_deviation: 12.56,
+ *      interquartile_standard_deviation: 12.45,
+ *                    median            : 12345,
+ *      interquartile_range             : 12,
+ *                    durations         : [ undefined, 12345, null, ... ],
+ *      interquartile_durations         : [ 10000, 10001 ... 19998, 19999 ],
+ *    }
+ */
+class DiaryStandard extends DiaryBase {
+
+    /**
+     * @param {Object} file - file contents, or object containing records
+     * @param {Array=} file.records - individual records from the sleep diary
+     * @param {number=} file.minimum_day_duration - minimum expected day duration in milliseconds
+     * @param {number=} file.maximum_day_duration - maximum expected day duration in milliseconds
+     * @param {Function=} serialiser - function to serialise output
+     */
+    constructor(file,serialiser) {
+
+        super(file,serialiser);
+
+        if ( file["records"] && !file["file_format"] ) {
+            file = {
+                "file_format": () => "Standard",
+                "contents"   : file,
+            };
+        }
+
+        switch ( file["file_format"]() ) {
+
+        case "url":
+            return this.initialise_from_url(file);
+
+        case "archive": // unsupported
+
+            return this.invalid(file);
+
+        case "string":
+            file = {
+                "file_format": () => "Standard",
+                "contents": /** @type (Object|null) */ (JSON.parse(file["contents"])),
+            }
+            if ( file["contents"]["file_format"] != "Standard" ) {
+                return this.invalid(file);
+            }
+            // FALL THROUGH
+
+        default:
+
+            let contents = file["contents"];
+            if (
+                file["file_format"]() != "Standard" ||
+                contents === null ||
+                typeof(contents) != "object" ||
+                !Array.isArray(contents["records"])
+            ) {
+                return this.invalid(file);
+            }
+
+            /**
+             * Individual records from the sleep diary
+             *
+             * @type Array<DiaryStandardRecord>
+             */
+            this["records"] = contents["records"];
+
+            /**
+             * Minimum expected length for a day
+             *
+             * <p>We calculate day numbers by looking for "asleep"
+             * records at least this far apart.</p>
+             *
+             * @type number
+             */
+            this["minimum_day_duration"] = contents["minimum_day_duration"] || 20*60*60*1000;
+
+            /**
+             * Maximum expected length for a day
+             *
+             * <p>We calculate skipped days by looking for "asleep"
+             * records at this far apart</p>
+             *
+             * @type number
+             */
+            this["maximum_day_duration"] = contents["maximum_day_duration"] || this["minimum_day_duration"]*2;
+
+            /*
+             * Calculate extra information
+             */
+            let day_start = 0,
+                day_number = 0,
+                prev = {
+                    "status": "",
+                    "day_number": -1
+                },
+                day_sleeps = [],
+                sleep_wake_record = prev
+            ;
+            const minimum_day_duration = this["minimum_day_duration"],
+                  maximum_day_duration = this["maximum_day_duration"]
+            ;
+
+            contents["records"]
+                .sort( (a,b) => ( a["start"] - b["start"] ) || ( a["end"] - b["end"] ) )
+                .forEach( r => {
+
+                    if ( !r.hasOwnProperty("duration") ) {
+                        r["duration"] = r["end"] - r["start"];
+                        if ( isNaN(r["duration"]) ) r["duration"] = null;
+                    }
+
+                    if ( r.hasOwnProperty("start_of_new_day") ) {
+                        if ( r["start_of_new_day"] ) {
+                            day_start = r["start"];
+                        }
+                    } else {
+                        r["start_of_new_day"] =
+                            r["status"] == "asleep" &&
+                            r["start"] > day_start + minimum_day_duration
+                        ;
+                    }
+
+                    if ( r.hasOwnProperty("day_number") ) {
+                        day_number = r["day_number"];
+                    } else {
+                        if ( r["start_of_new_day"] ) {
+                            if ( r["start"] > day_start + maximum_day_duration ) {
+                                // assume we skipped a day
+                                day_number += 2;
+                            } else {
+                                day_number += 1;
+                            }
+                            day_start = r["start"];
+                        }
+                        r["day_number"] = day_number;
+                    }
+
+                    if (  r["status"] == "awake" || r["status"] == "asleep" ) {
+                        if ( !sleep_wake_record.hasOwnProperty("missing_record_after") ) {
+                            sleep_wake_record["missing_record_after"] = (
+                                r["status"] == sleep_wake_record["status"]
+                            );
+                        }
+                        sleep_wake_record = r;
+                    }
+
+                    if ( r["status"] == "asleep" ) {
+
+                        if ( (day_sleeps[r["day_number"]]||{"duration":0})["duration"] < r["duration"] ) {
+                            day_sleeps[r["day_number"]] = r;
+                        }
+                    }
+
+                    prev = r;
+
+                })
+            ;
+
+            day_sleeps.forEach( r => {
+                if ( r && !r.hasOwnProperty("is_primary_sleep") ) r["is_primary_sleep"] = true;
+            });
+
+        }
+
+    }
+
+    ["to"](to_format) {
+
+        switch ( to_format ) {
+
+        case "output":
+            return this.serialise({
+                "file_format": () => "string",
+                "contents": JSON.stringify(Object.assign({"file_format":this["file_format"]()},this)),
+            });
+
+        default:
+            return super["to"](to_format);
+
+        }
+
+    }
+
+    ["merge"](other) {
+        let records = {};
+        [ this, other["to"](this["file_format"]()) ].forEach(
+            f => f["records"].forEach(
+                r => records[[ r["start"], r["end"], r["status"] ].join()] = r
+            )
+        );
+        this["records"] = Object.values(records).sort( (a,b) => ( a["start"] - b["start"] ) || ( a["end"] - b["end"] ) );
+        return this;
+    }
+
+    ["file_format"]() { return "Standard"; }
+
+    /**
+     * Internal function used by summarise_*
+     * @private
+     */
+    static summarise(durations) {
+
+        let defined_durations = durations.filter( r => r ),
+            total_durations   = defined_durations.length
+        ;
+
+        if ( !total_durations ) return null;
+
+        let a_plus_b        = (a,b) => a+b,
+            a_minus_b       = (a,b) => a-b,
+            sum_of_squares  = (a,r) => a + Math.pow(r - mean, 2) ,
+
+            sorted_durations  = defined_durations.sort(a_minus_b),
+            interquartile_durations = sorted_durations.slice(
+                Math.round( sorted_durations.length*0.25 ),
+                Math.round( sorted_durations.length*0.75 ),
+            ),
+
+            mean,
+                untrimmed_mean = defined_durations.reduce(a_plus_b) / (total_durations||1),
+            interquartile_mean = interquartile_durations.reduce(a_plus_b) / (interquartile_durations.length||1),
+
+            ret = {
+                            "average": untrimmed_mean,
+                               "mean": untrimmed_mean,
+                 "interquartile_mean": interquartile_mean,
+
+                             "median": sorted_durations[Math.floor(sorted_durations.length/2)],
+                "interquartile_range": (
+                    interquartile_durations[interquartile_durations.length-1] -
+                    interquartile_durations[0]
+                ),
+
+                              "durations":               durations,
+                "interquartile_durations": interquartile_durations,
+        };
+
+        // calculate standard deviations:
+        mean = untrimmed_mean;
+        ret["standard_deviation"] = Math.sqrt( defined_durations.reduce(sum_of_squares,0) / total_durations );
+        mean = interquartile_mean;
+        ret["interquartile_standard_deviation"] = Math.sqrt( interquartile_durations.reduce(sum_of_squares,0) / interquartile_durations.length );
+
+        return ret;
+
+    }
+
+    /**
+     * Summary statistics (based on individual records)
+     *
+     * <p>Because real-world data tends to be quite messy, and because
+     * different users have different requirements, we provide several
+     * summaries for the data:</p>
+     *
+     * <ul>
+     *  <li><tt>average</tt> is the best guess at what the
+     *      user would intuitively consider the average duration of a
+     *      record.  The exact calculation is chosen from the list
+     *      below, and may change in future.  It is currently the
+     *      <tt>trimmed_mean</tt>.  If you don't have any specific
+     *      requirements, you should use this and ignore the
+     *      others.</li>
+     *  <li><tt>mean</tt> and <tt>standard_deviation</tt> are
+     *      traditional summary statistics for the duration, but are
+     *      not recommended because real-world data tends to skew
+     *      these values higher than one would expect.</li>
+     *  <li><tt>interquartile_mean</tt> and <tt>interquartile_standard_deviation</tt>
+     *      produce more robust values in cases like ours, because they
+     *      ignore the highest and lowest few records.
+     *  <li><tt>median</tt> and <tt>interquartile_range</tt> produce
+     *      more robust results, but tend to be less representative when
+     *      there are only a few outliers in the data.
+     *  <li><tt>durations</tt> and <tt>interquartile_durations</tt>
+     *      are the raw values the other statistics were created from.
+     * </ul>
+     *
+     * @nocollapse
+     * @public
+     *
+     * @param {function(*)=} filter - only examine records that match this filter
+     *
+     * @return {null|{
+     *                 average           : number,
+     *                 mean              : number,
+     *   interquartile_mean              : number,
+     *                 standard_deviation: number,
+     *   interquartile_standard_deviation: number,
+     *                 median            : number,
+     *   interquartile_range             : number,
+     *                 durations         : Array<number|undefined>,
+     *   interquartile_durations         : Array<number|undefined>
+     * }}
+     *
+     */
+    ["summarise_records"](filter) {
+
+        return DiaryStandard.summarise(
+            ( filter ? this["records"].filter(filter) : this["records"] )
+                .map( r => r["duration"] )
+        );
+
+    }
+
+    /**
+     * Summary statistics (based on records grouped by day_number)
+     *
+     * <p>Similar to {@link DiaryStandard#summarise_records}, but
+     * groups records by day_number.</p>
+     *
+     * @nocollapse
+     * @public
+     *
+     * @see [summarise_records]{@link DiaryStandard#summarise_records}
+     * @tutorial Graph your day lengths
+     * 
+     * @param {function(*)=} filter - only examine records that match this filter
+     *
+     * @return {null|{
+     *                 average           : number,
+     *                 mean              : number,
+     *   interquartile_mean              : number,
+     *                 standard_deviation: number,
+     *   interquartile_standard_deviation: number,
+     *                 median            : number,
+     *   interquartile_range             : number,
+     *                 durations         : Array<number|undefined>,
+     *   interquartile_durations         : Array<number|undefined>
+     * }}
+     *
+     */
+    ["summarise_days"](filter) {
+
+        // get the earliest start time for each day:
+        let starts = [];
+        ( filter ? this["records"].filter(filter) : this["records"] )
+            .forEach( r => {
+                if ( starts[r["day_number"]]||0 < r["start"] )
+                    starts[r["day_number"]] = r["start"];
+            });
+
+        // remove leading undefined start times:
+        while ( starts.length && !starts[0] ) {
+            starts.shift();
+        }
+
+        // calculate day duration relative to previous day:
+        let durations = [];
+        for ( let n=1; n<starts.length; ++n ) {
+            if ( starts[n] && starts[n-1] ) {
+                durations[n-1] = starts[n] - starts[n-1];
+            }
+        }
+
+        return DiaryStandard.summarise(durations);
+
+    }
+
+    /**
+     * Latest sleep/wake status
+     *
+     * @nocollapse
+     * @public
+     * 
+     * @return {string} "awake", "asleep" or "" (for an empty diary)
+     */
+    ["latest_sleep_status"]() {
+
+        for ( let n=this["records"].length-1; n>=0; --n ) {
+            let status = this["records"][n].status;
+            if ( status == "awake" || status == "asleep" ) return status;
+        }
+        return "";
+
+    }
+
+}
+
+DiaryBase.register({
+    "name": "Standard",
+    "constructor": DiaryStandard,
+    "title": "Standardised diary format",
+    "url": "/src/Standard",
+});
