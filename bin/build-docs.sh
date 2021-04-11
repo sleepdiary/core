97fd6a7335e7e3dddaed1dcd83680e16b068c9e6
Andrew Sayers <andrew-github.com@pileofstuff.org>
2021-04-11 15:34:18 +0100

    Remove background execution in build-docs.sh
    (requires a tty for some reason, which GitHub Actions doesn't provide)

--- bin/build-docs.sh
+++ bin/build-docs.sh
@@ -5,13 +5,12 @@
 # Called from .github/workflows/gh-pages
 
 set -e # exit if any of the commands below return non-zero
-set -m # enable background jobs
 
 #
 # Initialise the build environment
 #
 
-npm install -g jsdoc google-closure-compiler &
+npm install -g jsdoc google-closure-compiler
 
 git clone --depth 1 https://github.com/wolfcw/libfaketime.git /tmp/libfaketime
 sed -i -e 's/\/usr\/local/\/tmp\/libfaketime/' /tmp/libfaketime/Makefile /tmp/libfaketime/*/Makefile
@@ -24,12 +23,6 @@ ln -s src /tmp/libfaketime/faketime
 #
 
 git merge --strategy-option=theirs --no-edit origin/main
-
-#
-# Finish initialisation
-#
-
-fg
 
 #
 # Run the build itself
