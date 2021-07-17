682512292ece519dbc8baadf3cce7b9122889144
Andrew Sayers <sleepdiary@pileofstuff.org>
2021-07-17 10:31:22 +0100

    Use the new project-wide e-mail address
    Actual command:
    
    	sed -i -e 's/andrew-github.com/sleepdiary/g' $( git grep -l andrew-github.com )

--- src/export.js
+++ src/export.js
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 Andrew Sayers <andrew-github.com@pileofstuff.org>
+ * Copyright 2020 Andrew Sayers <sleepdiary@pileofstuff.org>
  *
  * Permission is hereby granted, free of charge, to any person
  * obtaining a copy of this software and associated documentation
