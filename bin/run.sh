184c1d88503462372b2db71719d0d05716526535
Andrew Sayers <sleepdiary@pileofstuff.org>
2021-08-29 13:22:20 +0100

    Standardise the build process

--- bin/run.sh
+++ bin/run.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+set -e
+
+if [ -e /opt/sleepdiary/utils.sh ]
+then . /opt/sleepdiary/utils.sh
+else printf '\033[1;31m/opt/sleepdiary/utils.sh not found - some checks bypassed.\033[0m\n'
+fi
+
 do_build() {
 
     echo
@@ -11,16 +18,10 @@ do_build() {
 
 }
 
-if [ -e /opt/sleepdiary/utils.sh ]
-then . /opt/sleepdiary/utils.sh
-else printf '\033[1;31m/opt/sleepdiary/utils.sh not found - some checks bypassed.\033[0m\n'
-fi
-
 case "$1" in
 
     build)
 
-        npm ci --silent
         do_build
         ;;
 
